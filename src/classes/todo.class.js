6fcd6f4458a2445735800b6c74a337ea47ff646d
geminway92 <geminway@live.com>
2021-08-09 17:00:53 +0200

    Primer commit

---
+++ src/classes/todo.class.js
@@ -0,0 +1,27 @@
+import { todoList } from "..";
+
+export class Todo {
+
+    static fromJson({ id, tarea, completado, creado }) {
+            
+        const tempTodo = new Todo( tarea );
+
+        tempTodo.id        = id;
+        tempTodo.completado = completado;
+        tempTodo.creado     = creado;
+
+        return tempTodo;
+    }
+
+
+    constructor( tarea) {
+
+
+        this.tarea = tarea;
+
+        this.id         = new Date().getTime(); // 128368125
+        this.completado = false;
+        this.creado     = new Date();
+    }
+
+}
